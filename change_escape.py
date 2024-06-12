1cac08b35b1661e8a24184d1dce8e335245b4185
DArtagnant <105509558+DArtagnant@users.noreply.github.com>
2024-06-12 21:38:46 +0200

    improve perf #20

--- change_escape.py
+++ change_escape.py
@@ -1,7 +1,9 @@
+import functools
 from pygments.formatters import HtmlFormatter
 
 class HtmlFormatterSpecialEscape(HtmlFormatter):
     #We disable this function
+    @functools.lru_cache(maxsize=100)
     def _translate_parts(self, value):
         """old : HTML-escape a value and split it by newlines."""
         return value.split('\n')
