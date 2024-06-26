20189d887713a11a0b92c37a80be856e7641623f
DArtagnant <105509558+DArtagnant@users.noreply.github.com>
2024-06-26 21:07:49 +0200

    update build.bat

--- build.bat
+++ build.bat
@@ -1,10 +1,11 @@
 @echo off
 setlocal
 
-echo execution de pyinstaller...
-pyinstaller --name "BlueJ-code-highlighter" --specpath ".\build" --onefile --noconsole ".\gui.py" > output_build.txt 2>&1
+if not exist .\build mkdir .\build
+echo execution de pyinstaller... (prend du temps la premiere fois)
+pyinstaller --name "BlueJ-code-highlighter" --specpath ".\build" --onefile --noconsole ".\gui.py" > .\build\output_build.txt 2>&1
 
-findstr "error: (225," output.txt > nul
+findstr "error: (225," .\build\output_build.txt > nul
 if %errorlevel% equ 0 (
     echo WARNING la compilation d'une application graphique avec pyinstaller necessite de rajouter le dossier du projet en liste blanche
     echo executer de nouveau ce script apres ajout du chemin '%~dp0'
@@ -13,8 +14,8 @@ if %errorlevel% equ 0 (
 ) else (
     echo compilation terminee, execution
     explorer .\dist\
-    .\dist\gui.exe
+    start "" .\dist\BlueJ-code-highlighter.exe
 )
-del output_build.txt
+del .\build\output_build.txt
 
 endlocal
