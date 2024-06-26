9dc880f7664d32a41b2f9546dc84c044a7dcb327
DArtagnant <105509558+DArtagnant@users.noreply.github.com>
2024-06-26 20:21:36 +0200

    new .bat

--- build.bat
+++ build.bat
@@ -1,4 +1,19 @@
-python setup.py build
-copy .\LICENSE .\build\exe.win-amd64-3.11\
-copy .\README.md .\build\exe.win-amd64-3.11\
-explorer .\build\exe.win-amd64-3.11\
+@echo off
+setlocal
+
+echo execution de pyinstaller...
+pyinstaller --onefile --noconsole ".\gui.py" > output_build.txt 2>&1
+
+findstr "error: (225," output.txt > nul
+if %errorlevel% equ 0 (
+    echo WARNING la compilation d'une application graphique avec pyinstaller necessite de rajouter le dossier du projet en liste blanche
+    echo executer de nouveau ce script apres ajout du chemin '%~dp0'
+    echo ouverture de Windows Defender
+    powershell -command "Start-Process 'windowsdefender://threatsettings'"
+) else (
+    echo compilation terminee, execution
+    .\dist\gui.exe
+)
+del output_build.txt
+
+endlocal
